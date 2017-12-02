21cd5d9488942e12fe61a50f351c947e00502495
dmqueiroz <dmqueiroz@gmail.com>
2017-12-02 15:36:47 -0200

    Adding first program

---
+++ capitulo_02/bom_dia.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Programa Bom Dia !!!
+
+Created on Sat Dec  2 15:30:09 2017
+
+@author: Daniel Marçal de Queiroz
+"""
+#
+#
+nome = input("Qual é o seu nome?\n")
+print("\nBom dia, ",nome, "!!!!!")
+
